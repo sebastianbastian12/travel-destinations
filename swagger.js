469959b2eb91797732a9c2657003512d6b6b7dc2
Se-Bastian12 <sebastian.oliveros12@gmail.com>
2024-12-07 21:02:40 -0500

    "coming to original"

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'API interface documentation for Travel Destination Site',
     version: '1.0.0',
   },
-  host: 'localhost:3012/',
+  host: 'localhost:3012',
   schemes: ['http', 'https'],
 };
 
