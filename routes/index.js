0cf2be0cafd75adb3704884ec34b9458e09b23f1
Se-Bastian12 <sebastian.oliveros12@gmail.com>
2024-12-12 11:42:51 -0500

    "again"

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.use('/destinations', require('./destinations.r'));
 router.use('/destinationPlans', require('./destinationPlans.r'));
 router.use('/clients', require('./clients.r'));
 
-/*router.get('/login', passport.authenticate('github'), (req, res) => {});
+router.get('/login', passport.authenticate('github'), (req, res) => {});
 
 router.get('/logout', function (req, res, next) {
   req.logOut(function (err) {
@@ -16,6 +16,6 @@ router.get('/logout', function (req, res, next) {
     }
     res.redirect('/');
   });
-});*/
+});
 
 module.exports = router;
